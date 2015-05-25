ee7948432fe80873f6201863f4f100ef7efa47ed
Sanjay Rabidas <rabidassanjay@gmail.com>
2015-05-25 21:29:17 +0530

    Add: method to pull and render albums

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -16,14 +16,12 @@ function testAPI() {
     console.log('response ' , response);
     console.log('Successful login for: ' + response.name);
     document.getElementById('user_name').innerHTML = response.name;
-
-    document.getElementById('status').innerHTML =
-      'Thanks for logging in, ' + response.name + '!';
+    document.getElementById('status').innerHTML ='Thanks for logging in, ' + response.name + '!';
   });
 }
 
 
-function getPhotos(response){
+function getPhotos(){
   FB.api(
       "/me/photos",
       {access_token: access_token},
@@ -40,6 +38,44 @@ function getPhotos(response){
   );
 }
 
+function getAlbums(){
+  FB.api(
+      "/me/albums",
+      {access_token: access_token},
+      function (response) {
+        if (response && !response.error) {
+          /* handle the result */
+          console.log("albums data", response);
+          // populate(response);
+          populateAlbums(response);
+        }else{
+          console.log("albums :err", response);
+        }
+
+      }
+  );
+}
+
+function getEachAlbum(albumId){
+  FB.api(
+      albumId,
+      {access_token: access_token},
+      function (response) {
+        if (response && !response.error) {
+          /* handle the result */
+          console.log("data of Album ",albumId, response);
+          var c = document.getElementById('photo-container');
+          c.innerHTML = "";
+          // populate(response);
+        }else{
+          console.log("albums :err", response);
+        }
+
+      }
+  );
+}
+
+
 function login(){
   FB.login(function(response){
     console.log('Login');
@@ -51,7 +87,8 @@ function login(){
     if (response.status === 'connected') {
       // Logged into your app and Facebook.
       testAPI();
-      getPhotos(response);
+      // getAlbums();
+      getPhotos();
     } else if (response.status === 'not_authorized') {
       // The person is logged into Facebook, but not your app.
       document.getElementById('status').innerHTML = 'Please log ' +
@@ -65,9 +102,25 @@ function login(){
   },{scope:"public_profile,email,user_photos"});
 }
 
+function populateAlbums(response){
+
+  _.each(response.data, function(obj){
+    var button = document.createElement("button");
+    button.className = "btn btn-info pull-left";
+    button.dataset["id"] = obj.id;
+    button.innerHTML = obj.name + "("+obj.count+")";
+    var c = document.getElementById('album-container');
+    c.appendChild(button);
+    $(button).unbind().bind("click", function(loadAlbum){
+      var album_id = loadAlbum.currentTarget.getAttribute("data-id");
+      getEachAlbum(album_id);
+    });
+  });
+}
+
 function populate(response) {
   // $(document).ready(function(){
-    var c = document.getElementById('container');
+    var c = document.getElementById('photo-container');
     //
     // for(var i = 16;i > 0; i--){
     //   var item = document.createElement("div");
@@ -88,7 +141,7 @@ function populate(response) {
       // item.height = Math.ceil(Math.random()*100) +"px";
       c.appendChild(item);
     });
-    var container = document.querySelector('#container');
+    var container = document.querySelector('#photo-container');
     var msnry = new Masonry( container, {
       // options...
       itemSelector: '.item',
@@ -121,7 +174,4 @@ function loadNextPage(next){
       populate(response);
     }
   });
-}
-function renderPagination(pagingData){
-
 }
